be9d8c57193f8d11dce267210d2a49c45fdc8c65
黄健 <jian.huang03@ele.me>
2016-11-25 16:49:01 +0800

    Type: 调整类型声明。

---
+++ core.type.js
@@ -0,0 +1,11 @@
+/**
+ * @description - rollup-plugin-ng-annotate structure definition
+ * @author - bornkiller <hjj491229492@hotmail.com>
+ */
+
+/**
+ * @typedef {object} AnnotateRef
+ *
+ * @property {string} name - the function or class name
+ * @property {string} dependency - injector dependency list
+ */
